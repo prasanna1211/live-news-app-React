37becad366dc3a5a15e12af9bc944d25a0d69867
prasanna1211 <prasanna.krystal@gmail.com>
2017-02-28 18:23:29 +0530

    changed route and separated

---
+++ src/client/app/scripts/modules/singleSourceNews/SingleSourceNews.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SingleSourceNews = () => (
+  <div>
+    Single Source News
+  </div>
+);
+
+export default SingleSourceNews;
--- src/client/app/scripts/routes.jsx
+++ src/client/app/scripts/routes.jsx
@@ -7,13 +7,13 @@ import { Router, Route, browserHistory } from 'react-router';
 // import layout
 import Layout from 'scripts/modules/layout/Layout.jsx';
 
-// Composed routes for demo module
-import DemoRoutes from 'scripts/modules/demo/routes.jsx';
+// Composed routes for Single Source module
+import SingleSourceNewsRoutes from 'scripts/modules/singleSourceNews/routes.jsx';
 
 const Routes = () => (
   <Router history={browserHistory}>
     <Route component={Layout}>
-      { DemoRoutes }
+      { SingleSourceNewsRoutes }
     </Route>
   </Router>
 );
