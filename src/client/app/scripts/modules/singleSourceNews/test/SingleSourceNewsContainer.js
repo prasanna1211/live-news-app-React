b1e1692aaa89c7971da0c8401468a918ed706f2f
prasanna1211 <prasanna.krystal@gmail.com>
2017-03-02 16:17:53 +0530

    refined tests

--- src/client/app/scripts/modules/singleSourceNews/test/SingleSourceNewsContainer.js
+++ src/client/app/scripts/modules/singleSourceNews/test/SingleSourceNewsContainer.js
@@ -1,15 +1,30 @@
 /* global it, describe */
 
-import React from 'react';
-import { shallow } from 'enzyme';
+// import React from 'react';
+// import { shallow } from 'enzyme';
 import { expect } from 'chai';
+import configureStore from 'redux-mock-store';
+import _ from 'underscore';
 
-import SingleSourceNewsContainer from '../components/container/SingleSourceNewsContainer.jsx';
+// import SingleSourceNewsContainer from '../components/container/SingleSourceNewsContainer.jsx';
+import { sourceFetchActionCreators } from '../actionCreators/actionCreators.js';
 
-describe(' <SingleSourceNewsContainer /> component ', () => {
-  it(' renders correctly ', () => {
-    const wrapper = shallow(<SingleSourceNewsContainer />);
-    console.log(wrapper.debug());
-    expect(true);
+const middlewares = [];
+const mockStore = configureStore(middlewares);
+
+// Check if it dispatches correct action to the store
+describe(' <SingleSourceNewsContainer /> Component ', () => {
+  it(' should dispatch actions ', () => {
+    const initialState = {};
+    const store = mockStore(initialState);
+    store.dispatch(sourceFetchActionCreators());
+    const recievedAction = store.getActions();
+    const expectedAction = {
+      type: 'SOURCE_LIST_FETCH_REQUEST',
+      payload: {},
+    };
+    expect(_.isEqual(recievedAction, [expectedAction])).is.equal(true);
   });
 });
+
+// Todo: write tests for the entire container
