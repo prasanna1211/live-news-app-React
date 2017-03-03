751da9bf3259a92ac03afc77d3012fddc512a3af
prasanna1211 <prasanna.krystal@gmail.com>
2017-03-03 16:46:30 +0530

    added reducer tests

---
+++ src/client/app/scripts/modules/singleSourceNews/test/reducers.js
@@ -0,0 +1,45 @@
+/* global it, describe, expect */
+import Immutable from 'immutable';
+import { expect } from 'chai';
+import { sourceListReducer } from '../reducers/sourceListReducer.js';
+import { newsReducer } from '../reducers/newsReducer.js';
+
+describe(' #reducer:sourceListReducer ', () => {
+  it(' changes state correctly ', () => {
+    const initialState = Immutable.Map();
+    const actualNewState = sourceListReducer(initialState, {
+      type: 'SOURCE_LIST_FETCH_SUCCESS',
+      payload: {
+        test: 'test',
+      },
+    });
+    const expectedNewState = {
+      data: {
+        test: 'test',
+      },
+      sourceListFetchInitiated: true,
+      sourceListFetchSuccess: true,
+    };
+    expect(actualNewState.toJS()).to.deep.equal(expectedNewState);
+  });
+});
+
+describe(' #reducer:newsReducer ', () => {
+  it(' changes state correctly ', () => {
+    const initialState = Immutable.Map();
+    const actualNewState = newsReducer(initialState, {
+      type: 'NEWS_FETCH_REQUEST_SUCCESS',
+      payload: {
+        test: 'test',
+      },
+    });
+    const expectedNewState = {
+      data: {
+        test: 'test',
+      },
+      newsFetchInitiated: true,
+      newsFetchSuccess: true,
+    };
+    expect(actualNewState.toJS()).to.deep.equal(expectedNewState);
+  });
+});
